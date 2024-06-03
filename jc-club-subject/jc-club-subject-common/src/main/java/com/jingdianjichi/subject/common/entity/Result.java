385c09b9c89618bbad8e5d11f3cf28fdf5028d87
jay <1085484144@qq.com>
2024-06-03 19:44:41 +0800

    重构：为Controller层的统一返回体添加泛型支持

--- jc-club-subject/jc-club-subject-common/src/main/java/com/jingdianjichi/subject/common/entity/Result.java
+++ jc-club-subject/jc-club-subject-common/src/main/java/com/jingdianjichi/subject/common/entity/Result.java
@@ -4,7 +4,7 @@ import com.jingdianjichi.subject.common.enums.ResultCodeEnum;
 import lombok.Data;
 
 @Data
-public class Result {
+public class Result<T> {
 
     private Boolean success;
 
@@ -12,41 +12,76 @@ public class Result {
 
     private String message;
 
-    private Object data;
+    private T data;
 
     private String reqUuid;
 
+    public Result(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Result(String code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
     public static Result success() {
-        Result result = new Result();
+        Result result = new Result(ResultCodeEnum.SUCCESS.getCode(), ResultCodeEnum.SUCCESS.getMessage());
         result.setSuccess(true);
-        result.setCode(ResultCodeEnum.SUCCESS.getCode());
-        result.setMessage(ResultCodeEnum.SUCCESS.getMessage());
         return result;
     }
 
-    public static <T> Result success(T data) {
-        Result result = success();
+    public static Result success(String message) {
+        Result result = new Result(ResultCodeEnum.SUCCESS.getCode(), message);
+        result.setSuccess(true);
+        return result;
+    }
+
+    public static <T> Result<T> success(T data) {
+        Result<T> result = new Result<>(ResultCodeEnum.SUCCESS.getCode(), ResultCodeEnum.SUCCESS.getMessage(), data);
+        result.setSuccess(true);
+        result.setData(data);
+        return result;
+    }
+
+    public static <T> Result<T> success(String message, T data) {
+        Result<T> result = new Result<>(ResultCodeEnum.SUCCESS.getCode(), message, data);
+        result.setSuccess(true);
+        result.setData(data);
+        return result;
+    }
+
+    public static <T> Result<T> success(String code, String message, T data) {
+        Result<T> result = new Result<>(code, message, data);
+        result.setSuccess(true);
         result.setData(data);
         return result;
     }
 
     public static Result fail() {
-        Result result = new Result();
+        Result result = new Result(ResultCodeEnum.FAIL.getCode(), ResultCodeEnum.FAIL.getMessage());
         result.setSuccess(false);
-        result.setCode(ResultCodeEnum.FAIL.getCode());
-        result.setMessage(ResultCodeEnum.FAIL.getMessage());
         return result;
     }
 
-    public static <T> Result fail(String message) {
+    public static Result fail(String message) {
         Result result = fail();
         result.setMessage(message);
         return result;
     }
 
-    public static <T> Result fail(String message, T data) {
-        Result result = fail();
-        result.setMessage(message);
+    public static <T> Result<T> fail(String message, T data) {
+        Result<T> result = new Result<>(ResultCodeEnum.FAIL.getCode(), message, data);
+        result.setSuccess(false);
+        result.setData(data);
+        return result;
+    }
+
+    public static <T> Result<T> fail(String code, String message, T data) {
+        Result<T> result = new Result<>(code, message, data);
+        result.setSuccess(false);
         result.setData(data);
         return result;
     }
