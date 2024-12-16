2a86751c930a0cfe28fdf73c674d7b638c8ccac1
jay <1085484144@qq.com>
2024-12-16 18:54:49 +0800

    功能：打印所有执行的SQL语句

---
+++ jc-club-subject/jc-club-subject-infra/src/main/java/com/jingdianjichi/subject/infra/config/MybatisConfiguration.java
@@ -0,0 +1,20 @@
+package com.jingdianjichi.subject.infra.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfiguration {
+
+    /**
+     * 打印所有SQL的完整语句
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new MybatisPlusAllSqlLog());
+        return mybatisPlusInterceptor;
+    }
+
+}
