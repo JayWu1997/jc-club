041f9356c63b3787be41bd8a899519914564b3b7
jay <1085484144@qq.com>
2024-12-20 18:57:23 +0800

    Auth-功能：将服务注册到Nacos

--- jc-club-auth/jc-club-auth-starter/src/main/java/com/jingdianjichi/auth/AuthApplication.java
+++ jc-club-auth/jc-club-auth-starter/src/main/java/com/jingdianjichi/auth/AuthApplication.java
@@ -1,4 +1,4 @@
-package com.jingdianjichi.auth.start;
+package com.jingdianjichi.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
