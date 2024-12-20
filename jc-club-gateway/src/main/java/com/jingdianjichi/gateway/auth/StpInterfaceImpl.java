af9297132c0c1ff8290f8da3e3d73daec0aa0866
jay <1085484144@qq.com>
2024-12-21 00:29:19 +0800

    Gateway-功能：引入redis实现用户鉴权

--- jc-club-gateway/src/main/java/com/jingdianjichi/gateway/auth/StpInterfaceImpl.java
+++ jc-club-gateway/src/main/java/com/jingdianjichi/gateway/auth/StpInterfaceImpl.java
@@ -1,9 +1,13 @@
 package com.jingdianjichi.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.google.gson.Gson;
+import com.jingdianjichi.gateway.redis.RedisUtil;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,20 +16,45 @@ import java.util.List;
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    private static final Gson GSON = new Gson();
+
+    private static final String AUTH_PERMISSION_PREFIX = "auth.permission";
+
+    private static final String AUTH_ROLE_PREFIX = "auth.role";
+
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
-        // 返回此 loginId 拥有的权限列表
-        List<String> permissionList = new ArrayList<>();
-        permissionList.add("subject:add");
-        return permissionList;
+        return getStringListByKeyFromRedis(loginId.toString(), AUTH_PERMISSION_PREFIX);
     }
 
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
-        // 返回此 loginId 拥有的角色列表
-        List<String> roleList = new ArrayList<>();
-        roleList.add("admin");
-        return roleList;
+        return getStringListByKeyFromRedis(loginId.toString(), AUTH_ROLE_PREFIX);
+    }
+
+    /**
+     * 根据key从redis中获取string类型的list
+     * @param key key
+     * @param prefix 类型前缀
+     * @return list
+     */
+    private List<String> getStringListByKeyFromRedis(String key, String prefix) {
+        // 验证输入参数
+        if (StringUtils.isBlank(key) || StringUtils.isBlank(prefix)) {
+            return Collections.emptyList();
+        }
+
+        String authKey = redisUtil.buildKey(prefix, key);
+        String token = redisUtil.get(authKey);
+
+        if (StringUtils.isBlank(token)) {
+            return Collections.emptyList();
+        }
+
+        return GSON.fromJson(token, new com.google.gson.reflect.TypeToken<List<String>>() {}.getType());
     }
 
 }
