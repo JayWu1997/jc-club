d56610131051e159ad03d0cc7b6519cdc3783a29
jay <1085484144@qq.com>
2024-12-21 20:38:59 +0800

    Auth-功能：用户注册时绑定默认角色

--- jc-club-auth/jc-club-auth-domain/src/main/java/com/jingdianjichi/auth/domain/service/impl/AuthUserDomainServiceImpl.java
+++ jc-club-auth/jc-club-auth-domain/src/main/java/com/jingdianjichi/auth/domain/service/impl/AuthUserDomainServiceImpl.java
@@ -2,15 +2,25 @@ package com.jingdianjichi.auth.domain.service.impl;
 
 import cn.dev33.satoken.secure.SaSecureUtil;
 import com.jingdianjichi.auth.common.enums.IsDeletedEnum;
+import com.jingdianjichi.auth.common.enums.ResultCodeEnum;
 import com.jingdianjichi.auth.common.enums.UserEnableEnum;
+import com.jingdianjichi.auth.common.util.ParamCheckUtil;
 import com.jingdianjichi.auth.domain.converter.AuthUserBOConverter;
 import com.jingdianjichi.auth.domain.entity.AuthUserBO;
 import com.jingdianjichi.auth.domain.service.AuthUserDomainService;
+import com.jingdianjichi.auth.infra.base.entity.AuthRole;
 import com.jingdianjichi.auth.infra.base.entity.AuthUser;
+import com.jingdianjichi.auth.infra.base.entity.AuthUserRole;
+import com.jingdianjichi.auth.infra.base.service.AuthRoleService;
+import com.jingdianjichi.auth.infra.base.service.AuthUserRoleService;
 import com.jingdianjichi.auth.infra.base.service.AuthUserService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.Optional;
+
+import static com.jingdianjichi.auth.domain.constant.AuthConstant.DEFAULT_ROLE_KEY;
 
 /**
  * 用户领域服务实现
@@ -24,6 +34,12 @@ public class AuthUserDomainServiceImpl implements AuthUserDomainService {
     @Resource
     private AuthUserService authUserService;
 
+    @Resource
+    private AuthUserRoleService authUserRoleService;
+
+    @Resource
+    private AuthRoleService authRoleService;
+
     /**
      * 密码加密盐
      */
@@ -35,16 +51,35 @@ public class AuthUserDomainServiceImpl implements AuthUserDomainService {
      * @return token
      */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public String register(AuthUserBO authUserBO) {
-        AuthUser authUser = AuthUserBOConverter.INSTANCE.convertBo2Entity(authUserBO);
-        authUser.setStatus(UserEnableEnum.ENABLE.getCode());
-        authUser.setPassword(SaSecureUtil.md5BySalt(authUser.getPassword(), SALT));
-        authUser.setIsDeleted(IsDeletedEnum.NOT_DELETED.getCode());
-        authUserService.insert(authUser);
+        // 插入用户信息
+        AuthUser user = AuthUserBOConverter.INSTANCE.convertBo2Entity(authUserBO);
+        user.setStatus(UserEnableEnum.ENABLE.getCode());
+        user.setPassword(SaSecureUtil.md5BySalt(user.getPassword(), SALT));
+        user.setIsDeleted(IsDeletedEnum.NOT_DELETED.getCode());
+        authUserService.insert(user);
+        ParamCheckUtil.checkNotNull(user.getId(), ResultCodeEnum.PARAM_ERROR, "角色添加失败！");
 
-        //TODO 建立角色关联
+        // TODO 考虑是否改为从 redis取默认角色
+        // 获取默认角色
+        AuthRole role = new AuthRole();
+        role.setRoleKey(DEFAULT_ROLE_KEY);
+        role = Optional.ofNullable(authRoleService.queryAll(role))
+                .map(authRoles -> authRoles.get(0))
+                .orElse(null);
+        ParamCheckUtil.checkNotNull(role, ResultCodeEnum.PARAM_ERROR, "角色添加失败！role key" + DEFAULT_ROLE_KEY + "所代表的角色不存在!");
+
+        // 建立角色关联
+        AuthUserRole userRole = new AuthUserRole();
+        userRole.setUserId(user.getId());
+        userRole.setRoleId(role.getId());
+        userRole.setIsDeleted(IsDeletedEnum.NOT_DELETED.getCode());
+        authUserRoleService.insert(userRole);
+        ParamCheckUtil.checkNotNull(userRole.getId(), ResultCodeEnum.PARAM_ERROR, "角色添加失败！");
+
+        //TODO 把当前角色和权限刷到 redis中并生成 token
         String token = "";
-        //TODO 把当前角色和权限刷到redis中
         return token;
     }
 
